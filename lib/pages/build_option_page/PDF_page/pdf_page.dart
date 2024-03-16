54dc9693d8ef4541ece70e48eec186efd7911f85
Aneri09 <aneridobariya9@gmail.com>
2024-03-16 11:41:48 +0530

    pdf

--- lib/pages/build_option_page/PDF_page/pdf_page.dart
+++ lib/pages/build_option_page/PDF_page/pdf_page.dart
@@ -1,6 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
+
+import '../../../utils/Global.dart';
 
 class PDFPage extends StatefulWidget {
   const PDFPage({super.key});
@@ -10,8 +15,33 @@ class PDFPage extends StatefulWidget {
 }
 
 class _PDFPageState extends State<PDFPage> {
+  Future<Uint8List> getPdf() async {
+    pw.Document pdf = pw.Document();
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Center(
+          child: pw.Column(
+            children: [
+              pw.Text('hello'),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    return pdf.save();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PDF'),
+      ),
+      body: PdfPreview(
+        build: (format) => getPdf(),
+      ),
+    );
   }
 }
